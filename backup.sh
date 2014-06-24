f0dfaf25bc2164adc46df3c535b0a67c3e55befb
Mark Rose <markrose@markrose.ca>
2014-06-24 03:57:15 -0400

    Switch to separate script for backup.

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# read for backup host name
+source ~/.ec2-factorer.sh
+
+# instance id for backup directory
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+rsync $1/{M*,results*,worktodo.txt} $BACKUP_HOST:mfaktc_backup/$instance_id-$2/
