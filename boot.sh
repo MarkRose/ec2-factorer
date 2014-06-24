24b805d7dbfb9ad5d77976df07d730a481b7ae16
Mark Rose <markrose@markrose.ca>
2014-06-23 23:57:11 -0400

    Initial stab at creating needed scripts.

---
+++ boot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# read for backup host name
+source ~/.ec2-factorer.sh
+
+# update mfloopy
+cd ~/primetools
+git reset --hard
+git pull
+
+# update ec2-factorer
+cd ~/ec2-factorer
+git reset --hard
+git pull
+
+# instance type will determine number of GPUs to configure.
+instance_type=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+
+if [ "x$instance_type" == "xcg1.4xlarge" ] ; then last_card=1 ; else last_card=0 ; fi
+
+# pull some work and start processing immediately
+for i in $(seq 0 $last_card) ; do
+~/ec2-factorer/fetch.sh ~/mfaktc$i
+cd ~/mfaktc$i
+screen -dmS mf$i ./mfaktc.exe -d $i 
+done
+
+# create crontab
+cron=""
+for i in $(seq 0 $last_card) ; do
+cron="$cron
+30 * * * * $HOME/ec2-factorer/fetch.sh $HOME/mfaktc$i
+* * * * * rsync \"$HOME/mfaktc$i/{M*,results*,worktodo.txt}\" $BACKUP_HOST:mfaktc_backup/$INSTANCE_ID-$i/
+
+"
+echo $cron | crontab -
---
+++ fetch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# read credentials
+. ~/.mfloopy_credentials.sh
+
+~/primetools/mfloop.py -u $M_USER -p $M_PASS -U $G_USER -P $G_PASS -w $1 -t 0 -g 100 -e 74 -T lltf -o let_gpu72_decide
