b149f259c3cc7113949ff3953ca46406c8ce56e7
Mark Rose <markrose@markrose.ca>
2014-06-24 23:34:11 -0400

    Delete old files from backup directory; kills checkpoint files. But this time rsync the whole directory so it actually works :P

--- backup.sh
+++ backup.sh
@@ -6,4 +6,4 @@ source ~/.ec2-factorer.sh
 # instance id for backup directory
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-rsync -aq --delete $1/{M*,results*,worktodo.txt} $BACKUP_HOST:mfaktc_backup/$instance_id-$2/
+rsync -aq --delete --exclude=mfaktc.exe $1/ $BACKUP_HOST:mfaktc_backup/$instance_id-$2/
