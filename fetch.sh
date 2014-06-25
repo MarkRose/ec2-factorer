1a912677be4ea2593bc444d1f52f9554bc26ed1a
Mark Rose <markrose@markrose.ca>
2014-06-24 23:22:55 -0400

    Reduce the amount of work fetched to 2 hours-ish.

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,4 @@
 # read credentials
 . ~/.mfloopy_credentials.sh
 
-~/primetools/mfloop.py -u $M_USER -p $M_PASS -U $G_USER -P $G_PASS -w $1 -t 0 -g 100 -e 74 -T dctf -o let_gpu72_decide
+~/primetools/mfloop.py -u $M_USER -p $M_PASS -U $G_USER -P $G_PASS -w $1 -t 0 -g 25 -e 74 -T dctf -o let_gpu72_decide
